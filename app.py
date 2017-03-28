96c79f1c746d00c529648839bbfc9b3456634715
Stephanie Peña <stephaniebpena@gmail.com>
2017-03-27 18:16:22 -0700

    added results table to web app

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import pandas as pd
+import numpy as np
 from flask import Flask, render_template, request, jsonify
 app = Flask(__name__)
 
@@ -9,6 +11,30 @@ def index():
 @app.route('/dontfret', methods=['GET'])
 def pedal():
     return render_template('pedal.html')
+
+def get_data():
+    with open('data/cleaned_pedal_data.csv') as f:
+        pedal_data = pd.read_csv(f)
+    clean_pedal_data = pedal_data.drop('equipment_name',axis=1)
+    clean_pedal_data = clean_pedal_data.drop_duplicates()
+    clean_pedal_data.reset_index(drop=True,inplace=True)
+    table_df = clean_pedal_data.drop('cleaned_name',axis=1)
+    table_df['equipment_url'] = 'http://equipboard.com' + table_df['equipment_urls']
+    table_df = table_df.drop('equipment_urls',axis=1)
+    df = table_df.head(20)
+    return df
+
+df = get_data()
+brand_list = list(df['brand_name'])
+product_list = list(df['product_name'])
+cat_list = list(df['product_category'])
+instrument_list = list(df['instrument_type'])
+url_list = list(df['equipment_url'])
+
+# options = []
+#
+# for value in sorted(values.keys()):
+#     options.append("<option value='" + value + "'>" + values[value] + "</option>")
 
 @app.route('/')
 def brand_list():
--- static/js/app.js
+++ static/js/app.js
@@ -129,3 +129,70 @@ $( function() {
         }
       });
 } );
+
+jQuery(function($) {
+    var locations = {
+        'Boss': ['DS-1', 'TU-3', 'PS-2'],
+        'Spain': ['Barcelona'],
+        'Hungary': ['Pecs'],
+        'USA': ['Downers Grove'],
+        'Mexico': ['Puebla'],
+        'South Africa': ['Midrand'],
+        'China': ['Beijing'],
+        'Russia': ['St. Petersburg'],
+    }
+
+    var $locations = $('#pedalname');
+    $('#brand').change(function () {
+        var country = $(this).val(), lcns = locations[country] || [];
+
+        var html = $.map(lcns, function(lcn){
+            return '<option value="' + lcn + '">' + lcn + '</option>'
+        }).join('');
+        $locations.html(html)
+    });
+});
+
+
+$(function() {
+  $("#addMore").click(function(e) {
+    e.preventDefault();
+    $('#linedrop').clone().appendTo("#ui-widget");
+  });
+});
+
+var options = $python_list;
+var brand_list = $python_list;
+var product_list = $python_list;
+var cat_list = $python_list;
+var instrument_list = $python_list;
+var url_list = $python_list;
+
+
+	var props = {
+		filters_row_index: 1,
+		sort: true,
+		sort_config: {
+			sort_types:['String','String','US','US','US','US','US','US','US']
+		},
+		remember_grid_values: true,
+		alternate_rows: true,
+		rows_counter: true,
+		rows_counter_text: "Displayed rows: ",
+		btn_reset: true,
+		btn_reset_text: "Clear",
+		btn_text: " > ",
+		loader: true,
+		loader_text: "Filtering data...",
+		loader_html: '<img src="loader.gif" alt="" ' +
+				'style="vertical-align:middle;" /> Loading...',
+		on_show_loader: hideIESelects, //IE only: selects are hidden when loader visible
+		on_hide_loader: showIESelects, //IE only: selects are displayed when loader closed
+		col_0: "select",
+		col_1: "select",
+		col_2: "select",
+		col_9: "none",
+		display_all_text: "< Show all >",
+		col_width: ["15%","10%","10%","10%","13%","10%","12%","10%","10%"]
+	}
+	var tf = setFilterGrid("demo",props);
