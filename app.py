ad8e2ad2776e6018e9135cb4825af1501570279d
Stephanie Peña <stephaniebpena@gmail.com>
2017-03-28 21:31:41 -0700

    added filter functionality to results table

--- app.py
+++ app.py
@@ -10,7 +10,13 @@ def index():
 
 @app.route('/dontfret', methods=['GET'])
 def pedal():
-    return render_template('pedal.html')
+    df = get_data()
+    df = df.to_html(index=False)
+    df = df.replace('class="dataframe">','id="data-types" class="dataframe>"')
+    return render_template('pedal.html',tables=[df])
+# @app.route('/test', methods=['GET'])
+# def test():
+#     return render_template('test.html')
 
 def get_data():
     with open('data/cleaned_pedal_data.csv') as f:
@@ -18,23 +24,33 @@ def get_data():
     clean_pedal_data = pedal_data.drop('equipment_name',axis=1)
     clean_pedal_data = clean_pedal_data.drop_duplicates()
     clean_pedal_data.reset_index(drop=True,inplace=True)
-    table_df = clean_pedal_data.drop('cleaned_name',axis=1)
+    # table_df = clean_pedal_data.drop('cleaned_name',axis=1)
+    table_df = clean_pedal_data
     table_df['equipment_url'] = 'http://equipboard.com' + table_df['equipment_urls']
     table_df = table_df.drop('equipment_urls',axis=1)
-    df = table_df.head(20)
+    # df = table_df.iloc[100:200]
+    pedal_list = ['TC Electronic Ditto Looper Limited Gold Edition',
+     'Ibanez TS9 Tube Screamer',
+     'EarthQuaker Devices Ghost Echo Reverb',
+     'TC Electronic Hall Of Fame Reverb',
+     'Strymon El Capistan dTape Echo Delay',
+     'Voodoo Lab Power 2 Plus',
+     'Line 6 POD HD500',
+     'Roger Mayer Voodoo Vibe',
+     'Moog Minifooger MF Chorus',
+     'Boss DD-3 Digital Delay',
+     'MXR M108 Ten Band Graphic Equalizer',
+     'Musonic VP100 Phaser',
+     'Boss TU-3 Chromatic Tuner',
+     'Top Tone Drivegate DG1',
+     'Electro-Harmonix Micro POG',
+     'Fulltone OCD Obsessive Compulsive Drive Overdrive',
+     'Empress Compressor']
+    df = table_df[table_df['cleaned_name'].isin(pedal_list)]
+    df = df.drop('cleaned_name',axis=1)
+    df = df.rename({'brand_name': 'Brand','product_name': 'Pedal','product_category': 'Category','instrument_type': 'Instrument','equipment_url': 'Equipboard URL'})
     return df
 
-df = get_data()
-brand_list = list(df['brand_name'])
-product_list = list(df['product_name'])
-cat_list = list(df['product_category'])
-instrument_list = list(df['instrument_type'])
-url_list = list(df['equipment_url'])
-
-# options = []
-#
-# for value in sorted(values.keys()):
-#     options.append("<option value='" + value + "'>" + values[value] + "</option>")
 
 @app.route('/')
 def brand_list():
--- static/js/app.js
+++ static/js/app.js
@@ -130,11 +130,11 @@ $( function() {
       });
 } );
 
-jQuery(function($) {
-    var locations = {
+$(function pedalmap() {
+    var mapping = {
         'Boss': ['DS-1', 'TU-3', 'PS-2'],
-        'Spain': ['Barcelona'],
-        'Hungary': ['Pecs'],
+        'TC Electronic': ['Ditto Looper'],
+        'Electro-Harmonix': ['Small Clone','Small Stone','Hall of Fame'],
         'USA': ['Downers Grove'],
         'Mexico': ['Puebla'],
         'South Africa': ['Midrand'],
@@ -142,57 +142,26 @@ jQuery(function($) {
         'Russia': ['St. Petersburg'],
     }
 
-    var $locations = $('#pedalname');
-    $('#brand').change(function () {
-        var country = $(this).val(), lcns = locations[country] || [];
 
+
+    var $mapping = $('#pedalname');
+    $('#brand').change(function () {
+        var brand = $(this).val(), lcns = mapping[brand] || [];
         var html = $.map(lcns, function(lcn){
             return '<option value="' + lcn + '">' + lcn + '</option>'
         }).join('');
-        $locations.html(html)
+        $mapping.html(html)
     });
 });
 
+// show recommendations table after button click
+function showTable() {
+    document.getElementById("table-data").style.display="none";
+    document.getElementById("table-data").style.display="block";
+}
 
-$(function() {
-  $("#addMore").click(function(e) {
-    e.preventDefault();
-    $('#linedrop').clone().appendTo("#ui-widget");
-  });
+// add more pedal data
+$("#addMore").click(function(){
+    $(".linedrop:first").clone(true).append('<button id="remove">-</button>').appendTo(".ui-widget");
+    $(".linedrop:last").empty();
 });
-
-var options = $python_list;
-var brand_list = $python_list;
-var product_list = $python_list;
-var cat_list = $python_list;
-var instrument_list = $python_list;
-var url_list = $python_list;
-
-
-	var props = {
-		filters_row_index: 1,
-		sort: true,
-		sort_config: {
-			sort_types:['String','String','US','US','US','US','US','US','US']
-		},
-		remember_grid_values: true,
-		alternate_rows: true,
-		rows_counter: true,
-		rows_counter_text: "Displayed rows: ",
-		btn_reset: true,
-		btn_reset_text: "Clear",
-		btn_text: " > ",
-		loader: true,
-		loader_text: "Filtering data...",
-		loader_html: '<img src="loader.gif" alt="" ' +
-				'style="vertical-align:middle;" /> Loading...',
-		on_show_loader: hideIESelects, //IE only: selects are hidden when loader visible
-		on_hide_loader: showIESelects, //IE only: selects are displayed when loader closed
-		col_0: "select",
-		col_1: "select",
-		col_2: "select",
-		col_9: "none",
-		display_all_text: "< Show all >",
-		col_width: ["15%","10%","10%","10%","13%","10%","12%","10%","10%"]
-	}
-	var tf = setFilterGrid("demo",props);
